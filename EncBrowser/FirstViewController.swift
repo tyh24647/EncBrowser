1ef65ad00b8c97b7fa3ce3afb417790299a500ed
tyh24647 <tyhostager@yahoo.com>
2017-02-27 18:18:40 -0600

    Initial Commit

---
+++ EncBrowser/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  EncBrowser
+//
+//  Created by Tyler hostager on 2/27/17.
+//  Copyright © 2017 Tyler Hostager. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
